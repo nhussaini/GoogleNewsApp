97c0d5680232c380bdbed37642f14f75f3ddcce5
nhussaini <nasrullah_hussaini@yahoo.com>
2024-02-01 22:43:26 -0500

    create a loading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from 'react';
+import './App.css';
+import Loader from './components/Loading';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   return (
     <div className="App">
-      <header className="App-header">Welcome to SkillReactor!</header>
+      {/* <header className="App-header">Welcome to SkillReactor!</header> */}
+      {loading && <Loader />}
     </div>
   );
 }
